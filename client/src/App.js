b66d4594b4f427434fed46a04ffcbddfc910a928
Munshid <muhammadmunshid200@gmail.com>
2023-03-03 17:02:46 +0530

    added spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 function App() {
+  const {loading} = useSelector((state)=> state.alerts)
   return (
     <div className="App">
       <BrowserRouter>
+      {loading &&
+        <div className="spinner-parent">
+          <div class="spinner-border" role="status">
+            
+          </div>
+        </div>}
         <Toaster position="top-center" reverseOrder={false} />
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -3,15 +3,17 @@ import { Button, Form, Input } from 'antd'
 import { Link,useNavigate } from 'react-router-dom';
 import axios from "axios";
 import toast from 'react-hot-toast'
-import {useSelector,useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux';
+import { hideLoading, showLoading } from '../redux/alertsSlice';
 
 function Login() {
-    const {loading} = useSelector(state => state.alerts)
-    console.log(loading);
+    const dispatch = useDispatch()
     const navigate = useNavigate()
     const onFinish = async(values) => {
         try {
+            dispatch(showLoading())
             const response = await axios.post('/api/user/login',values)
+            dispatch(hideLoading())
             if (response.data.success) {
                 toast.success(response.data.message)
                 toast("Redirecting to home page")
@@ -21,6 +23,7 @@ function Login() {
                 toast.error(response.data.message)
             }
            } catch (error) {
+            dispatch(hideLoading())
             toast.success("Something went wrong")
            }
     }
